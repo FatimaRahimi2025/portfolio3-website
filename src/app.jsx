5579bef30f6f4dc7af468a8cd6bda82e13bcf74b
Fatima Rahimi <rahimifatima432@gmail.com>
2025-09-06 19:32:42 +0430

    create NavBar

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
+import NavBar from "./assets/Component/NavBar";
+
 function App(){
   return(
-    <h1 className="bg-red-500 text-center font-bold text-3xl hover:text-white p-4">Hellow world</h1>
+    <div>
+    <NavBar />
+    </div>
   )
 }
 
