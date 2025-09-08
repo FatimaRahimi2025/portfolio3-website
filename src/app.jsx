a26b51c2e547171390fdb40d386a8e01524431e0
Fatima Rahimi <rahimifatima432@gmail.com>
2025-09-08 19:07:11 +0430

    create portfolio

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,15 @@
 import NavBar from "./assets/Component/NavBar";
 import Home from "./assets/Component/Home"
+import About from "./assets/Component/About";
 import SocialLink from "./assets/Component/SocialLink";
+import Portfolio from "./assets/Component/Portfolio";
 function App(){
   return(
     <div>
     <NavBar />
     <Home />
+    <About />
+    <Portfolio />
     <SocialLink />
     </div>
   )
