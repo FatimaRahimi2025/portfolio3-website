913187feed4ea62a78169797a0cdcce8c91be405
Fatima Rahimi <rahimifatima432@gmail.com>
2025-09-10 16:26:29 +0430

    create contact

--- src/assets/Component/Contact.jsx
+++ src/assets/Component/Contact.jsx
@@ -2,7 +2,28 @@ import React from "react";
 
 const Contact = () => {
     return(
-        <div>Contact</div>
+        <div name="contact" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+            <div>
+                <div>
+                    <p>Contact</p>
+                    <p>Submit the form below to get in touch with me</p>
+                </div>
+
+                <div>
+                    <form action="" className="">
+                        <input type="text" name="name" placeholder="Enter your name"
+                        className="p-2 bg-transparent  border-2 rounded-md text-white focus:outline-none"/>
+
+                        <input type="text" name="email" placeholder="Enter your email"
+                        className="p-2 bg-transparent  border-2 rounded-md text-white focus:outline-none"/>
+                        <textarea name="message" rows="10"
+                        className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"></textarea>
+                        <button className="text-white bg-gradient-to-b from-syan-500 to-blue-500 px-6 py-3 my-8 mx-auto 
+                        flex items-center rounded-md hover:scale-110 duration-300"></button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Contact;
